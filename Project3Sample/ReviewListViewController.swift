8cf5ddc88d6b7ae1b9ce9dc297b33c2229f1c3b3
cramsayiv <37635288+cramsayiv@users.noreply.github.com>
2019-12-12 12:02:55 -0600

    complete assignment

--- Project3Sample/ReviewListViewController.swift
+++ Project3Sample/ReviewListViewController.swift
@@ -6,12 +6,39 @@
 //  Copyright © 2019 Russell Mirabelli. All rights reserved.
 //
 
-import UIKit
-
 class ReviewListViewController: UIViewController {
     
+    @IBOutlet weak var myReviews: UITableView!
     
+    let reviewService = ReviewService.shared
     
+    override func viewDidLoad() {
+        
+        fetchReviews()
+        myReviews.dataSource = self
+        
+    }
     
+    func fetchReviews () {
+        
+        reviewService.fetchReviews { [weak self] in
+            DispatchQueue.main.async {
+                self?.myReviews.reloadData()
+            }
+        }
+    }
+}
+
+extension ReviewListViewController: UITableViewDataSource {
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.reviewService.reviews.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let my_review = reviewService.reviews[indexPath.item]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ReviewCell") as! ReviewCell
+        cell.config (review: my_review, service: self.reviewService)
+        return cell
+    }
 }
