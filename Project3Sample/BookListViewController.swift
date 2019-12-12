1280311a46887f63adfa474d8bcb192e8bffa189
cramsayiv <37635288+cramsayiv@users.noreply.github.com>
2019-12-12 12:02:04 -0600

    complete assignment

--- Project3Sample/BookListViewController.swift
+++ Project3Sample/BookListViewController.swift
@@ -8,9 +8,46 @@
 
 import UIKit
 
-class BookListViewController: UIViewController {
+class BookListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+
+    @IBOutlet weak var refreshButton: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    
+    let bookService = BookService.shared
+    
+    override func viewDidLoad() {
+        fetchBooks()
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+    }
     
     
+    func fetchBooks() {
+        bookService.fetchBooks { [weak self] in
+            
+            DispatchQueue.main.async {
+                self?.tableView.reloadData()
+            }
+        }
+    }
     
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return self.bookService.books.count
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let my_book = bookService.books[indexPath.item]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BookCell") as! BookCell
+        cell.config (book: my_book, service: self.bookService)
+        
+        return cell
+        
+    }
 }
